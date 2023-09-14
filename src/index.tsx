eeac6bc97bce98ba11be2a8677e54cba0d571975
dante01yoon <bunggl@naver.com>
2023-09-14 13:51:30 +0900

    feat: add simple todo

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { ServeOptions } from "bun";
 import { Elysia } from "elysia";
+import { renderToString } from "react-dom/server";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
@@ -8,11 +9,14 @@ console.log(
 );
 
 
+type Todo = { id: number, text: string }
+const todos: Todo[] = [];
+
 const server = Bun.serve({
   hostname: "localhost",
   port: process.env.PORT,
   fetch: handler,
-  error(error) {
+  error() {
     return new Response("oops error occurred!!")
   },
 }) as ServeOptions
@@ -27,13 +31,30 @@ function  serveAsset(request: Request) {
   return new Response("Not Found", { status: 404});
 }
 
-function handler(request: Request): Response {
+async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
 
   if(url.pathname.startsWith('/assets')){
     return serveAsset(request)
   }
-  
+
+  if(url.pathname === "/todos" && request.method === "POST") {
+    const { todo } = await request.json();
+    
+    if(!todo?.length) return new Response('Invalid input', { status: 400})
+
+    todos.push({
+      id: todos.length + 1,
+      text: todo,
+    })
+
+    return new Response(renderToString(<TodoList todos={todos} key={todo.id}></TodoList>))
+  }
+
+  if(url.pathname === "/todos" && request.method === "GET") {
+    return new Response(renderToString(<TodoList todos={[]}></TodoList>));
+  }
+
   if(url.pathname === '' || url.pathname === '/')  {
     return new Response (Bun.file('index.html'));
   }
@@ -41,3 +62,10 @@ function handler(request: Request): Response {
 }
 
 console.log(`Listening on http://${server.hostname}:${server.port}`)
+
+
+function TodoList(props: { todos:  Todo[]}){
+  return <ul>
+    { props.todos.length ? props.todos.map(todo => <li>{todo.text}</li>) : "No items added"}
+  </ul>
+}
